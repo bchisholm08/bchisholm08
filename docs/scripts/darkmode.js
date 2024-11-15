23b528b782398c8097e027abb7144e62327c237a
Brady M. Chisholm <160765096+bchisholm08@users.noreply.github.com>
2024-11-15 09:48:06 -0600

    Update darkmode.js

--- docs/scripts/darkmode.js
+++ docs/scripts/darkmode.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector('.dark-mode-toggle');
+// Add event listener to toggle dark mode class on body
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
